781aa510108e76f2d5f190c1a257f33f0351409b
Caleb Hattingh <caleb.hattingh@gmail.com>
2019-04-11 20:33:05 +1000

    Bumped version to 2019.4.1

--- aiorun.py
+++ aiorun.py
@@ -24,7 +24,7 @@ from functools import partial
 
 
 __all__ = ['run', 'shutdown_waits_for']
-__version__ = '2019.3.1'
+__version__ = '2019.4.1'
 logger = logging.getLogger('aiorun')
 WINDOWS = sys.platform == 'win32'
 
