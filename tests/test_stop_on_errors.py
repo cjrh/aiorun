dca6c70e2b8e1c8734b9129b8b4337b1ab18e2fe
Fedir Alifirenko <fedor.alifirenko@gmail.com>
2020-02-12 00:53:23 +0200

    fixed KeyError when loop's exception handler was called with context without exception key (#43)

--- tests/test_stop_on_errors.py
+++ tests/test_stop_on_errors.py
@@ -26,3 +26,40 @@ def test_exc_stop():
     print(excinfo.traceback)
     assert "Stops the loop" in str(excinfo.value)
     assert all(t.cancelled for t in created_tasks)
+
+
+class _TestException(Exception):
+    pass
+
+
+@pytest.mark.parametrize('context, raised_exc', [
+    (
+        {'message': 'test error'},
+        None,
+    ),
+    (
+        {'message': 'test error', 'exception': _TestException('test error')},
+        _TestException,
+    ),
+])
+def test_call_exception_handler(context, raised_exc):
+    """Test when loop's exception handler was called with custom context"""
+    created_tasks = []
+
+    async def background_task():
+        await asyncio.sleep(2)
+
+    async def main():
+        loop = asyncio.get_event_loop()
+        created_tasks.extend(loop.create_task(background_task()) for _ in range(5))
+        await asyncio.sleep(0.1)
+        loop.call_exception_handler(context=context)
+
+    if raised_exc is not None:
+        with pytest.raises(raised_exc) as excinfo:
+            run(main(), stop_on_unhandled_errors=True)
+            assert "test error" in str(excinfo.value)
+    else:
+        run(main(), stop_on_unhandled_errors=True)
+
+    assert all(t.cancelled for t in created_tasks)
