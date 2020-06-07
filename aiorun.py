2d4822345a312971dc8ed0497d9a50310df2b6f8
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-06-07 14:51:58 +1000

    Version bump 2020.6.1

--- aiorun.py
+++ aiorun.py
@@ -18,7 +18,7 @@ from functools import partial
 
 
 __all__ = ["run", "shutdown_waits_for"]
-__version__ = "2020.2.1"
+__version__ = "2020.6.1"
 logger = logging.getLogger("aiorun")
 WINDOWS = sys.platform == "win32"
 
