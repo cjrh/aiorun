a837a0fa65edd6da4619a0761afc91fcdf458e3b
Caleb Hattingh <caleb.hattingh@gmail.com>
2023-07-05 14:03:28 +0200

    Update version to 2023.7.1

--- aiorun.py
+++ aiorun.py
@@ -21,7 +21,7 @@ ShutdownCallback = Optional[
 
 
 __all__ = ["run", "shutdown_waits_for"]
-__version__ = "2022.11.1"
+__version__ = "2023.7.1"
 logger = logging.getLogger("aiorun")
 WINDOWS = sys.platform == "win32"
 
